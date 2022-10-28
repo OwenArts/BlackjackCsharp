7cdb81e88baa7cafcf3107c3603bbbd797c4357f
Owen <owen.arts@hotmail.com>
2022-10-28 12:51:44 +0200

    Added Client with .net6.0

---
+++ Eindopdracht/Client/Client_.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using Common;
+
+namespace Client;
+
+public class Client_
+{
+    private readonly Log _log = new(typeof(Client_));
+    private const int Port = 7492;
+
+    public Client_()
+    {
+    }
+}
